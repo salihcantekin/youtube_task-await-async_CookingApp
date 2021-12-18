fa3edd7ebada6ec6aebabd87c3589a7e661f8f40
Salih Cantekin <salihcantekin@gmail.com>
2021-12-18 22:46:18 +0000

    Add project files.

---
+++ CookingApp.Desktop/frmMain.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace CookingApp.Desktop
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlButtons = new System.Windows.Forms.Panel();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.btnStep9 = new System.Windows.Forms.Button();
+            this.btnStep8 = new System.Windows.Forms.Button();
+            this.btnStep7 = new System.Windows.Forms.Button();
+            this.btnStep6 = new System.Windows.Forms.Button();
+            this.btnStep5 = new System.Windows.Forms.Button();
+            this.btnStep4 = new System.Windows.Forms.Button();
+            this.btnStep3 = new System.Windows.Forms.Button();
+            this.btnStep2 = new System.Windows.Forms.Button();
+            this.btnStep1 = new System.Windows.Forms.Button();
+            this.lbLogs = new System.Windows.Forms.ListBox();
+            this.btnStartCookingSync = new System.Windows.Forms.Button();
+            this.btnAsync = new System.Windows.Forms.Button();
+            this.pnlButtons.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlButtons
+            // 
+            this.pnlButtons.Controls.Add(this.lblTime);
+            this.pnlButtons.Controls.Add(this.btnStep9);
+            this.pnlButtons.Controls.Add(this.btnStep8);
+            this.pnlButtons.Controls.Add(this.btnStep7);
+            this.pnlButtons.Controls.Add(this.btnStep6);
+            this.pnlButtons.Controls.Add(this.btnStep5);
+            this.pnlButtons.Controls.Add(this.btnStep4);
+            this.pnlButtons.Controls.Add(this.btnStep3);
+            this.pnlButtons.Controls.Add(this.btnStep2);
+            this.pnlButtons.Controls.Add(this.btnStep1);
+            this.pnlButtons.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnlButtons.Location = new System.Drawing.Point(619, 0);
+            this.pnlButtons.Name = "pnlButtons";
+            this.pnlButtons.Size = new System.Drawing.Size(250, 508);
+            this.pnlButtons.TabIndex = 0;
+            // 
+            // lblTime
+            // 
+            this.lblTime.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblTime.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTime.Location = new System.Drawing.Point(0, 473);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(250, 35);
+            this.lblTime.TabIndex = 4;
+            this.lblTime.Text = "Timer";
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnStep9
+            // 
+            this.btnStep9.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnStep9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStep9.Location = new System.Drawing.Point(0, 376);
+            this.btnStep9.Name = "btnStep9";
+            this.btnStep9.Size = new System.Drawing.Size(250, 47);
+            this.btnStep9.TabIndex = 1;
+            this.btnStep9.Text = "Servis Et";
+            this.btnStep9.UseVisualStyleBackColor = true;
+            // 
+            // btnStep8
+            // 
+            this.btnStep8.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnStep8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStep8.Location = new System.Drawing.Point(0, 329);
+            this.btnStep8.Name = "btnStep8";
+            this.btnStep8.Size = new System.Drawing.Size(250, 47);
+            this.btnStep8.TabIndex = 2;
+            this.btnStep8.Text = "Pişir";
+            this.btnStep8.UseVisualStyleBackColor = true;
+            // 
+            // btnStep7
+            // 
+            this.btnStep7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnStep7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStep7.Location = new System.Drawing.Point(0, 282);
+            this.btnStep7.Name = "btnStep7";
+            this.btnStep7.Size = new System.Drawing.Size(250, 47);
+            this.btnStep7.TabIndex = 3;
+            this.btnStep7.Text = "Yumurtayı Ekle";
+            this.btnStep7.UseVisualStyleBackColor = true;
+            // 
+            // btnStep6
+            // 
+            this.btnStep6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnStep6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStep6.Location = new System.Drawing.Point(0, 235);
+            this.btnStep6.Name = "btnStep6";
+            this.btnStep6.Size = new System.Drawing.Size(250, 47);
+            this.btnStep6.TabIndex = 0;
+            this.btnStep6.Text = "Yağı Dök";
+            this.btnStep6.UseVisualStyleBackColor = true;
+            // 
+            // btnStep5
+            // 
+            this.btnStep5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnStep5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStep5.Location = new System.Drawing.Point(0, 188);
+            this.btnStep5.Name = "btnStep5";
+            this.btnStep5.Size = new System.Drawing.Size(250, 47);
+            this.btnStep5.TabIndex = 0;
+            this.btnStep5.Text = "Tavayı Isıt";
+            this.btnStep5.UseVisualStyleBackColor = true;
+            // 
+            // btnStep4
+            // 
+            this.btnStep4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnStep4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStep4.Location = new System.Drawing.Point(0, 141);
+            this.btnStep4.Name = "btnStep4";
+            this.btnStep4.Size = new System.Drawing.Size(250, 47);
+            this.btnStep4.TabIndex = 0;
+            this.btnStep4.Text = "Ocağı Aç";
+            this.btnStep4.UseVisualStyleBackColor = true;
+            // 
+            // btnStep3
+            // 
+            this.btnStep3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnStep3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStep3.Location = new System.Drawing.Point(0, 94);
+            this.btnStep3.Name = "btnStep3";
+            this.btnStep3.Size = new System.Drawing.Size(250, 47);
+            this.btnStep3.TabIndex = 0;
+            this.btnStep3.Text = "Tuz Ekle";
+            this.btnStep3.UseVisualStyleBackColor = true;
+            // 
+            // btnStep2
+            // 
+            this.btnStep2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnStep2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStep2.Location = new System.Drawing.Point(0, 47);
+            this.btnStep2.Name = "btnStep2";
+            this.btnStep2.Size = new System.Drawing.Size(250, 47);
+            this.btnStep2.TabIndex = 0;
+            this.btnStep2.Text = "Yumurtaları Çırp";
+            this.btnStep2.UseVisualStyleBackColor = true;
+            // 
+            // btnStep1
+            // 
+            this.btnStep1.BackColor = System.Drawing.SystemColors.Control;
+            this.btnStep1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnStep1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStep1.Location = new System.Drawing.Point(0, 0);
+            this.btnStep1.Name = "btnStep1";
+            this.btnStep1.Size = new System.Drawing.Size(250, 47);
+            this.btnStep1.TabIndex = 0;
+            this.btnStep1.Text = "Yumurtaları Kır";
+            this.btnStep1.UseVisualStyleBackColor = false;
+            // 
+            // lbLogs
+            // 
+            this.lbLogs.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lbLogs.FormattingEnabled = true;
+            this.lbLogs.ItemHeight = 20;
+            this.lbLogs.Location = new System.Drawing.Point(0, 284);
+            this.lbLogs.Name = "lbLogs";
+            this.lbLogs.Size = new System.Drawing.Size(619, 224);
+            this.lbLogs.TabIndex = 1;
+            // 
+            // btnStartCookingSync
+            // 
+            this.btnStartCookingSync.Location = new System.Drawing.Point(94, 45);
+            this.btnStartCookingSync.Name = "btnStartCookingSync";
+            this.btnStartCookingSync.Size = new System.Drawing.Size(427, 79);
+            this.btnStartCookingSync.TabIndex = 2;
+            this.btnStartCookingSync.Text = "Start Cooking Sync";
+            this.btnStartCookingSync.UseVisualStyleBackColor = true;
+            this.btnStartCookingSync.Click += new System.EventHandler(this.btnStartCookingSync_Click);
+            // 
+            // btnAsync
+            // 
+            this.btnAsync.Location = new System.Drawing.Point(94, 156);
+            this.btnAsync.Name = "btnAsync";
+            this.btnAsync.Size = new System.Drawing.Size(427, 79);
+            this.btnAsync.TabIndex = 2;
+            this.btnAsync.Text = "Start Cooking Async";
+            this.btnAsync.UseVisualStyleBackColor = true;
+            this.btnAsync.Click += new System.EventHandler(this.btnAsync_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(869, 508);
+            this.Controls.Add(this.btnAsync);
+            this.Controls.Add(this.btnStartCookingSync);
+            this.Controls.Add(this.lbLogs);
+            this.Controls.Add(this.pnlButtons);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Task async/await ";
+            this.pnlButtons.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlButtons;
+        private System.Windows.Forms.Button btnStep6;
+        private System.Windows.Forms.Button btnStep5;
+        private System.Windows.Forms.Button btnStep4;
+        private System.Windows.Forms.Button btnStep3;
+        private System.Windows.Forms.Button btnStep2;
+        private System.Windows.Forms.Button btnStep1;
+        private System.Windows.Forms.ListBox lbLogs;
+        private System.Windows.Forms.Button btnStep9;
+        private System.Windows.Forms.Button btnStep8;
+        private System.Windows.Forms.Button btnStep7;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Button btnStartCookingSync;
+        private System.Windows.Forms.Button btnAsync;
+    }
+}
---
+++ CookingApp.Desktop/frmMain.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CookingApp.Desktop
+{
+    public partial class frmMain : Form
+    {
+        private readonly System.Windows.Forms.Timer timer;
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+            timer = new System.Windows.Forms.Timer();
+            timer.Tick += (sender, e) =>
+            {
+                lblTime.Text = DateTime.Now.ToString("HH:mm:ss");
+            };
+
+            timer.Interval = 1000;
+            timer.Start();
+        }
+
+        /*
+            Omlet Tarifi;
+
+            1) Yumurtaları Kır
+            2) Yumurtaları Çırp
+            3) Tuz Ekle
+            4) Ocağı Aç
+            5) Tavayı Isıt
+            6) Yağı Dök
+            7) Yumurtayı Ekle
+            8) Pişir
+            9) Servis Et
+         */
+
+
+        private void btnStartCookingSync_Click(object sender, EventArgs e)
+        {
+            PrepareScreenForStarting();
+
+            var sw = Stopwatch.StartNew();
+
+            YumurtalariKir();
+            YumurtalariCirp();
+            TuzEkle();
+            OcagiAc();
+            TavayiIsit();
+            YagiDok();
+            YumurtayiEkle();
+            Pisir();
+            ServisEt();
+
+            sw.Stop();
+            AddLog();
+            AddLog($"Toplam Yemek Pişirme Süresi: {sw.ElapsedMilliseconds:0} MS");
+        }
+
+        private async void btnAsync_Click(object sender, EventArgs e)
+        {
+            PrepareScreenForStarting();
+
+            var sw = Stopwatch.StartNew();
+
+
+            var yumurtaTaskGroup = await YumurtalariKirAsync()
+                .ContinueWith(async _ =>
+                {
+                    await YumurtalariCirpAsync();
+                    await TuzEkleAsync();
+                }, TaskScheduler.FromCurrentSynchronizationContext());
+
+            var ocakTaskGroup = await OcagiAcAsync()
+                .ContinueWith(async _ =>
+                {
+                    await TavayiIsitAsync();
+                    await YagiDokAsync();
+                    await YumurtayiEkleAsync();
+                }, TaskScheduler.FromCurrentSynchronizationContext());
+
+            await Task.WhenAll(yumurtaTaskGroup, ocakTaskGroup);
+
+
+            await PisirAsync();
+            await ServisEtAsync();
+
+
+
+            sw.Stop();
+            AddLog();
+            AddLog($"Toplam Yemek Pişirme Süresi: {sw.ElapsedMilliseconds:0} MS");
+        }
+
+
+        #region Sync Methods
+
+        public void YumurtalariKir()
+        {
+            Task.Delay(500).Wait();
+            AddLog("Yumurtalar Kırıldı");
+            AdjustButtons(1);
+        }
+
+        public void YumurtalariCirp()
+        {
+            Task.Delay(750).Wait();
+            AddLog("Yumurtalar Çırpıldı");
+            AdjustButtons(2);
+
+            // 1) --------            ------------
+            // 2)         ------------
+        }
+
+        public void TuzEkle()
+        {
+            Task.Delay(200).Wait();
+            AddLog("Tuz Eklendi");
+            AdjustButtons(3);
+        }
+
+        public void OcagiAc()
+        {
+            Task.Delay(500).Wait();
+            AddLog("Ocak Açıldı");
+            AdjustButtons(4);
+        }
+
+        public void TavayiIsit()
+        {
+            Task.Delay(1000).Wait();
+            AddLog("Tava Isındı");
+            AdjustButtons(5);
+        }
+
+        public void YagiDok()
+        {
+            Task.Delay(750).Wait();
+            AddLog("Yağ Tavaya Döküldü");
+            AdjustButtons(6);
+        }
+
+        public void YumurtayiEkle()
+        {
+            Task.Delay(750).Wait();
+            AddLog("Yumurtalar Tavaya Döküldü");
+            AdjustButtons(7);
+        }
+
+        public void Pisir()
+        {
+            Task.Delay(2000).Wait();
+            AddLog("Yumurtalar Pişti");
+            AdjustButtons(8);
+        }
+
+        public void ServisEt()
+        {
+            Task.Delay(750).Wait();
+            AddLog("Yumurtalar Servis Edildi");
+            AdjustButtons(9);
+        }
+
+        #endregion
+
+        #region Async Methods
+
+        public async Task YumurtalariKirAsync()
+        {
+            await Task.Delay(500);
+            AddLog("Yumurtalar Kırıldı");
+            AdjustButtons(1);
+        }
+
+        public async Task YumurtalariCirpAsync()
+        {
+            await Task.Delay(750);
+            AddLog("Yumurtalar Çırpıldı");
+            AdjustButtons(2);
+        }
+
+        public async Task TuzEkleAsync()
+        {
+            await Task.Delay(200);
+            AddLog("Tuz Eklendi");
+            AdjustButtons(3);
+        }
+
+        public async Task OcagiAcAsync()
+        {
+            await Task.Delay(500);
+            AddLog("Ocak Açıldı");
+            AdjustButtons(4);
+        }
+
+        public async Task TavayiIsitAsync()
+        {
+            await Task.Delay(1000);
+            AddLog("Tava Isındı");
+            AdjustButtons(5);
+        }
+
+        public async Task YagiDokAsync()
+        {
+            await Task.Delay(750);
+            AddLog("Yağ Tavaya Döküldü");
+            AdjustButtons(6);
+        }
+
+        public async Task YumurtayiEkleAsync()
+        {
+            await Task.Delay(750);
+            AddLog("Yumurtalar Tavaya Döküldü");
+            AdjustButtons(7);
+        }
+
+        public async Task PisirAsync()
+        {
+            await Task.Delay(200);
+            AddLog("Yumurtalar Pişti");
+            AdjustButtons(8);
+        }
+
+        public async Task ServisEtAsync()
+        {
+            await Task.Delay(750);
+            AddLog("Yumurtalar Servis Edildi");
+            AdjustButtons(9);
+        }
+
+        #endregion
+
+
+
+        private void AddLog(string logStr = "")
+        {
+            if (!string.IsNullOrEmpty(logStr))
+                logStr = $"[{DateTime.Now:dd:MM.yyyy HH:mm:ss}] - {logStr}";
+
+            lbLogs.Items.Add(logStr);
+            lbLogs.TopIndex = lbLogs.Items.Count - 1; // Locate the last row
+        }
+
+        private void PrepareScreenForStarting()
+        {
+            foreach (Control control in pnlButtons.Controls)
+            {
+                if (control is Button btn)
+                    btn.BackColor = SystemColors.Control;
+            }
+
+            pnlButtons.Update();
+
+            lbLogs.Items.Clear();
+        }
+
+        private void AdjustButtons(int step)
+        {
+            Button btn = pnlButtons.Controls[$"btnStep{step}"] as Button;
+
+            btn.BackColor = Color.LimeGreen;
+        }
+    }
+}
